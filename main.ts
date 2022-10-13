56edffe778c31fb9b6a142b2355f4f4013d50073
Christian Mendez <112632376+Mendez002@users.noreply.github.com>
2022-10-13 11:08:15 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    if (input.compassHeading() == 0) {
+        basic.showArrow(ArrowNames.North)
+    } else if (input.compassHeading() == 45) {
+        basic.showArrow(ArrowNames.NorthEast)
+    } else if (input.compassHeading() == 90) {
+        basic.showArrow(ArrowNames.East)
+    } else if (input.compassHeading() == 135) {
+        basic.showArrow(ArrowNames.SouthEast)
+    } else if (input.compassHeading() == 180) {
+        basic.showArrow(ArrowNames.South)
+    } else if (input.compassHeading() == 225) {
+        basic.showArrow(ArrowNames.West)
+    } else if (input.compassHeading() == 270) {
+    	
+    } else if (input.compassHeading() == 315) {
+    	
+    } else if (false) {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
