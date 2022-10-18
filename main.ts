1950cae0923fa6005ab4e5f35fcc4d54adf11c3b
Christian Mendez <112632376+Mendez002@users.noreply.github.com>
2022-10-18 17:29:58 -0300

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,13 +10,11 @@ input.onButtonPressed(Button.A, function () {
     } else if (input.compassHeading() == 180) {
         basic.showArrow(ArrowNames.South)
     } else if (input.compassHeading() == 225) {
-        basic.showArrow(ArrowNames.West)
+        basic.showArrow(ArrowNames.SouthEast)
     } else if (input.compassHeading() == 270) {
-    	
+        basic.showArrow(ArrowNames.West)
     } else if (input.compassHeading() == 315) {
-    	
-    } else if (false) {
-    	
+        basic.showArrow(ArrowNames.NorthEast)
     }
 })
 input.onButtonPressed(Button.B, function () {
